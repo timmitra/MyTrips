dedf0a017a84f6abf5020be897d1cce032376b59
Tim Mitra <tim@it-guy.com>
2024-07-08 22:21:14 -0400

    embed list in nav stack, add destinations in List, plus inflect plurals.

--- MyTrips/Destinations Tab/DestinationsListView.swift
+++ MyTrips/Destinations Tab/DestinationsListView.swift
@@ -12,14 +12,38 @@ struct DestinationsListView: View {
     @Query(sort: \Destination.name) private var destinations: [Destination]
     
     var body: some View {
-        if !destinations.isEmpty {
-            Text("Hello, World!")
-        } else {
-            ContentUnavailableView(
-            "No Destinations",
-            systemImage: "globe.desk",
-            description: Text("You have not yet set up ant destinations yet. Tap on the \(Image(systemName: "plus.circle.fill")) button in the toolbar to begin.")
-            )
+        NavigationStack {
+            Group {
+                if !destinations.isEmpty {
+                    List(destinations) { destination in
+                        HStack {
+                            Image(systemName: "globe")
+                                .imageScale(.large)
+                                .foregroundStyle(.accent)
+                            VStack(alignment: .leading) {
+                                Text(destination.name)
+                                Text("^[\(destination.placemarks.count) location](inflect: true)")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                } else {
+                    ContentUnavailableView(
+                        "No Destinations",
+                        systemImage: "globe.desk",
+                        description: Text("You have not yet set up ant destinations yet. Tap on the \(Image(systemName: "plus.circle.fill")) button in the toolbar to begin.")
+                    )
+                }
+            }
+            .navigationTitle("My Destinations")
+            .toolbar {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                }
+            }
         }
     }
 }
