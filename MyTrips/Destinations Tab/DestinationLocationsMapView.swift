287f3bb01395886e5ed944d86325fb95e2c32f65
Tim Mitra <tim@it-guy.com>
2024-07-08 20:57:13 -0400

    markers, annotations and saving visible Region

--- MyTrips/Destinations Tab/DestinationLocationsMapView.swift
+++ MyTrips/Destinations Tab/DestinationLocationsMapView.swift
@@ -14,9 +14,48 @@ import MapKit
 
 struct DestinationLocationsMapView: View {
     @State private var cameraPosition: MapCameraPosition = .automatic
+    @State private var visibleRegion: MKCoordinateRegion
     var body: some View {
         Map(position: $cameraPosition) {
-            Marker("Moulin Trouge", coordinate: CLLocationCoordinate2D(latitude: 48.884134, longitude: 2.332196))
+            Marker("Moulin Trouge", coordinate:.moulinRouge)
+            Marker(coordinate: .arcDeTriomphe) {
+                Label("Arc De Triomphe", systemImage: "star.fill")
+            }
+            .tint(.yellow)
+            Marker("Eiffel Tower", image: "eiffelTower", coordinate: .eiffelTower)
+                .tint(.blue)
+            Marker("Gard du Nord", monogram: Text("GN"), coordinate: .gareDuNord)
+                .tint(.accent)
+            Marker("Louvre", systemImage: "person.crop.artframe", coordinate: .louvre)
+            Annotation("Notre Dame", coordinate: .notreDame) {
+                Image(systemName: "star")
+                    .imageScale(.large)
+                    .foregroundStyle(.red)
+                    .padding(10)
+                    .background(.white)
+                    .clipShape(.circle)
+            }
+            Annotation("Sacre Coeur", coordinate: .sacreCoeur, anchor: .center) {
+                Image(.sacreCoeur)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+            }
+            Annotation("Pantheon", coordinate: .pantheon) {
+                Image(systemName: "mappin")
+                    .imageScale(.large)
+                    .foregroundStyle(.red)
+                    .padding(5)
+                    .overlay {
+                        Circle()
+                            .strokeBorder(.red, lineWidth: 2)
+                    }
+            }
+            MapCircle(center: CLLocationCoordinate2D(latitude: 48.856788, longitude: 2.351077), radius: 5000)
+                .foregroundStyle(.red.opacity(0.25))
+        }
+        .onMapCameraChange(frequency: .onEnd) { context in
+            visibleRegion = context.region
         }
             .onAppear {
                 // 48.856788, 2.351077
