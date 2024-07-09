8bec797e800f213f2a3ab9344a31a7a75a96b931
Tim Mitra <tim@it-guy.com>
2024-07-08 22:05:59 -0400

    added DestinationListView

---
+++ MyTrips/Destinations Tab/DestinationsListView.swift
@@ -0,0 +1,30 @@
+//
+//  DestinationsListView.swift
+//  MyTrips
+//
+//  Created by Tim Mitra on 2024-07-08.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DestinationsListView: View {
+    @Query(sort: \Destination.name) private var destinations: [Destination]
+    
+    var body: some View {
+        if !destinations.isEmpty {
+            Text("Hello, World!")
+        } else {
+            ContentUnavailableView(
+            "No Destinations",
+            systemImage: "globe.dark",
+            description: Text("You have not yet set up ant destinations yet. Tap on the \(Image(systemName: "plus.circle.fill")) button in the toolbar to begin.")
+            )
+        }
+    }
+}
+
+#Preview {
+    DestinationsListView()
+        .modelContainer(Destination.preview)
+}
